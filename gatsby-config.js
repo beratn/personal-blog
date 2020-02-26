90226d9f7420c54f8595830157881d74480a6015
Berat Nakip <berat.nakip@kartaca.com>
2020-02-26 15:56:45 +0300

    added bio

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Kyle Matthews`,
+    title: `Software Developer`,
+    author: `Berat Nakip`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    siteUrl: `https://berat703.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `brtnkp`,
     },
   },
   plugins: [
